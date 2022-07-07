af057d37fa2bc6ede48edebe1e20207b2a72fd67
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-07-06 21:49:25 -0300

    db up

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+Match.init({
+  homeTeam: INTEGER,
+  homeTeamGoals: INTEGER,
+  awayTeam: INTEGER,
+  awayTeamGoals: INTEGER,
+  inProgress: BOOLEAN,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'match',
+  timestamps: false,
+});
+
+export default Match;
---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,24 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  username: STRING,
+  role: STRING,
+  email: STRING,
+  password: STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
