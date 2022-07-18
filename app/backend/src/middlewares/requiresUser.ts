7f232d764b2e3880a3181475565ad4ee921a6b79
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-07-18 13:08:02 -0300

    dev: Create middleware to validate login entries

---
+++ app/backend/src/middlewares/requiresUser.ts
@@ -0,0 +1,39 @@
+import * as Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+
+const isRequired = Joi.object({
+  username: Joi.string().min(8).max(256).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
+  role: Joi.string().length(6).required(),
+});
+
+const loginRequirer = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
+});
+
+const userDoc = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { username, email, password, role } = req.body;
+    Joi.assert({ username, email, password, role }, isRequired);
+    next();
+  } catch (err: any) {
+    return res.status(400).json({ message: err.details[0].message });
+  }
+};
+
+const loginDoc = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    Joi.assert({ email, password }, loginRequirer);
+    next();
+  } catch (err: any) {
+    return res.status(400).json({ message: err.details[0].message });
+  }
+};
+
+export default {
+  userDoc,
+  loginDoc,
+};
