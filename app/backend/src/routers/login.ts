fe1420efec6e6198d231e3c39ac5d07419c99711
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-07-23 19:32:50 -0300

    dev: att route to apply login

--- app/backend/src/routers/login.ts
+++ app/backend/src/routers/login.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import controlLogin from '../controllers/controlLogin';
 import validate from '../middlewares/requiresUser';
 
 const router = Router();
 
 router.get('/validate', (_req, res) => res.status(400).json({ message: 'loginPage' }));
-router.post('/', validate.loginDoc, (_req, res) => res.status(400).json({ message: 'loginPage' }));
+router.post('/', validate.loginDoc, controlLogin.login);
 
 export default router;
