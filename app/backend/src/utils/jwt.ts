f775f50a2d7edc823781bb4b8988bb7764809b57
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-07-23 19:30:54 -0300

    dev: import dotenv in utils/jwt

--- app/backend/src/utils/jwt.ts
+++ app/backend/src/utils/jwt.ts
@@ -1,11 +1,10 @@
 import jwt = require('jsonwebtoken');
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const SECRET_ = process.env.JWT_SECRET;
 const SECRET = SECRET_ as string;
-// const authConfig = {
-//   expiresIn: '1d',
-//   algorithm: 'HS256',
-// };
 
 const generateToken = (data = {}) => jwt.sign(
   { data },
