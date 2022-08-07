c5146b110711b10d34a1567aa7d3faa7598f7f4a
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-07 18:52:55 -0300

    dev: now Matches do data verifications

--- app/backend/src/routers/matches.ts
+++ app/backend/src/routers/matches.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import controlMatches from '../controllers/controlMatches';
 import authJwt from '../middlewares/authJwt';
+import matchDoc from '../middlewares/requiresMatch';
 
 const router = Router();
 
 router.get('/', controlMatches.loadMatches);
-router.post('/', authJwt, controlMatches.newMatch);
+router.post('/', authJwt, matchDoc.newMatchDoc, controlMatches.newMatch);
 router.patch('/:id/finish', authJwt, controlMatches.toEndMatch);
 
 export default router;
