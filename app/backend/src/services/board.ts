7704f256285a345196af40655755fa9d5a6e5554
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-08 17:12:35 -0300

    dev: retored board services

--- app/backend/src/services/board.ts
+++ app/backend/src/services/board.ts
@@ -2,6 +2,7 @@ import { ITeamHMatches, ITeamAMatches, ILeaderboard,
   ITeamAllMatches, ICalcTypes } from '../interfaces/ITable';
 import Matches from '../database/models/match';
 import Teams from '../database/models/team';
+import util from '../utils/calcBoard';
 
 // referencias https://github.com/tryber/sd-017-trybe-futebol-clube/pull/150/files
 const getHomeMatches = async (): Promise<ITeamHMatches[]> => {
@@ -51,127 +52,6 @@ const getDataMatches = async (): Promise<ITeamAllMatches[]> => {
   return allMatches as ITeamAllMatches[];
 };
 
-const calcGames = (oper: keyof ICalcTypes, team: ITeamAllMatches): number => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.length),
-    away: () => (team.awayMatches.length),
-    all: () => (team.homeMatches.length + team.awayMatches.length),
-  };
-  return result[oper]();
-};
-
-const calcVictories = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.reduce<number>((victories, match) => {
-      let wins = victories;
-      if (match.homeTeamGoals > match.awayTeamGoals) wins += 1;
-      return wins;
-    }, 0)),
-    away: () => (team.awayMatches.reduce<number>((victories, match) => {
-      let wins = victories;
-      if (match.awayTeamGoals > match.homeTeamGoals) wins += 1;
-      return wins;
-    }, 0)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcDraws = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.reduce<number>((draws, match) => {
-      let total = draws;
-      if (match.homeTeamGoals === match.awayTeamGoals) total += 1;
-      return total;
-    }, 0)),
-    away: () => (team.awayMatches.reduce<number>((draws, match) => {
-      let total = draws;
-      if (match.awayTeamGoals === match.homeTeamGoals) total += 1;
-      return total;
-    }, 0)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcPoints = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (calcVictories('home', team) * 3 + calcDraws('home', team)),
-    away: () => (calcVictories('away', team) * 3 + calcDraws('away', team)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcLoses = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.reduce<number>((loses, match) => {
-      let total = loses;
-      if (match.homeTeamGoals < match.awayTeamGoals) total += 1;
-      return total;
-    }, 0)),
-    away: () => (team.awayMatches.reduce<number>((loses, match) => {
-      let total = loses;
-      if (match.awayTeamGoals < match.homeTeamGoals) total += 1;
-      return total;
-    }, 0)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcGoalsFavor = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.reduce<number>((homeGoals, goals) => {
-      let total = homeGoals;
-      total += goals.homeTeamGoals;
-      return total;
-    }, 0)),
-    away: () => (team.awayMatches.reduce<number>((awayGoals, goals) => {
-      let total = awayGoals;
-      total += goals.awayTeamGoals;
-      return total;
-    }, 0)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcGoalsOwn = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (team.homeMatches.reduce<number>((homeGoals, goals) => {
-      let total = homeGoals;
-      total += goals.awayTeamGoals;
-      return total;
-    }, 0)),
-    away: () => (team.awayMatches.reduce<number>((awayGoals, goals) => {
-      let total = awayGoals;
-      total += goals.homeTeamGoals;
-      return total;
-    }, 0)),
-    all: () => (result.home() + result.away()),
-  };
-  return result[oper]();
-};
-
-const calcGoalsBalance = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => (calcGoalsFavor('home', team) - calcGoalsOwn('home', team)),
-    away: () => (calcGoalsFavor('away', team) - calcGoalsOwn('away', team)),
-    all: () => (result.home() - result.away()),
-  };
-  return result[oper]();
-};
-
-const calcEfficiency = (oper: keyof ICalcTypes, team: ITeamAllMatches) => {
-  const result: ICalcTypes = {
-    home: () => ((calcPoints('home', team) / (calcGames('home', team) * 3)) * 100),
-    away: () => ((calcPoints('away', team) / (calcGames('away', team) * 3)) * 100),
-    all: () => (result.home() - result.away()),
-  };
-  return result[oper]().toFixed(2);
-};
-
 const orderBoard = (board: ILeaderboard[]) => {
   const order = board.sort((a: any, b: any) => (
     b.totalPoints - a.totalPoints
@@ -189,15 +69,15 @@ const listBoard = async (oper: keyof ICalcTypes): Promise<ILeaderboard[]> => {
   const homeTeamsBoard = teams.map((team) => {
     const edit = {
       name: team.teamName,
-      totalPoints: calcPoints(oper, team),
-      totalGames: calcGames(oper, team),
-      totalVictories: calcVictories(oper, team),
-      totalDraws: calcDraws(oper, team),
-      totalLosses: calcLoses(oper, team),
-      goalsFavor: calcGoalsFavor(oper, team),
-      goalsOwn: calcGoalsOwn(oper, team),
-      goalsBalance: calcGoalsBalance(oper, team),
-      efficiency: parseFloat(calcEfficiency(oper, team)),
+      totalPoints: util.calcPoints(oper, team),
+      totalGames: util.calcGames(oper, team),
+      totalVictories: util.calcVictories(oper, team),
+      totalDraws: util.calcDraws(oper, team),
+      totalLosses: util.calcLoses(oper, team),
+      goalsFavor: util.calcGoalsFavor(oper, team),
+      goalsOwn: util.calcGoalsOwn(oper, team),
+      goalsBalance: util.calcGoalsBalance(oper, team),
+      efficiency: parseFloat(util.calcEfficiency(oper, team)),
     };
     return edit;
   });
