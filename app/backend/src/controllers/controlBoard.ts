004aef702fc733fc4bc66280993a0ec3d2c334cd
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-08 17:15:44 -0300

    dev: create routes to list Leaderboard

--- app/backend/src/controllers/controlBoard.ts
+++ app/backend/src/controllers/controlBoard.ts
@@ -8,6 +8,20 @@ const getHomeTeamBoard = async (req: Request, res: Response) => {
   return res.status(200).json(homeTeamBoard);
 };
 
+const getAwayTeamBoard = async (req: Request, res: Response) => {
+  const homeTeamBoard = await serviceBoard.listBoard('away');
+
+  return res.status(200).json(homeTeamBoard);
+};
+
+const getLeaderBoard = async (req: Request, res: Response) => {
+  const homeTeamBoard = await serviceBoard.listBoard('all');
+
+  return res.status(200).json(homeTeamBoard);
+};
+
 export default {
   getHomeTeamBoard,
+  getAwayTeamBoard,
+  getLeaderBoard,
 };
