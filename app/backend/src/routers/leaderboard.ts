6d53a3975f831e866553e8f057ba23e15eb0e175
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-08 17:15:44 -0300

    dev: create routes to list Leaderboard

--- app/backend/src/routers/leaderboard.ts
+++ app/backend/src/routers/leaderboard.ts
@@ -4,6 +4,8 @@ import controlBoard from '../controllers/controlBoard';
 const router = Router();
 
 router.get('/home', controlBoard.getHomeTeamBoard);
+router.get('/away', controlBoard.getHomeTeamBoard);
+router.get('/', controlBoard.getHomeTeamBoard);
 // router.get('/:id');
 
 export default router;
